546d4f85fe75253080c68a913082207d7b814a09
harrylovecoding <harrylovecoding@gmail.com>
2017-05-31 22:46:50 +0800

    Initial Commit

---
+++ Messager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messager
+//
+//  Created by Harry Cao on 31/5/17.
+//  Copyright © 2017 Harry Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
